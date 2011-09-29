bb0c8457e95aaf638d90552f33323a9d30de8439
yoorek <yoorek@gmail.com>
2011-09-30 00:08:43 +0200

    First Version

---
+++ app/app.js
@@ -0,0 +1,57 @@
+/**
+*    app.js	
+*    
+*    Created by Jerzy Błaszczyk on 2011-09-13.
+*    Copyright 2011 Client and Friends. All rights reserved.
+**/
+
+Ext.regApplication({
+
+    name: 'app',
+    tabletStartupScreen: 'images/tablet_startup.png',
+    phoneStartupScreen: 'images/phone_startup.png',
+    icon: 'images/icon.png',
+    glossOnIcon: true,
+
+    launch: function() {
+
+        this.launched = true;
+        this.mainLaunch();
+    },
+
+    mainLaunch: function() {
+
+        Ext.util.Format.defaultDateFormat = "F d, Y, h:i a";
+
+        Ext.is.Phone = true;
+
+        this.logging = false;
+        this.openViewport();
+    },
+
+    openViewport: function() {
+
+        this.viewport = new app.views.Viewport();
+    },
+
+    log: function(message) {
+        if (this.logging) console.log(message);
+    },
+
+    aop: function(object) {
+
+        for (i in object) {
+            var member = object[i];
+            if (typeof member === 'function') {
+                AOP.addBefore(function() {
+                    var obj = object.name,
+                    method = i;
+                    return function() {
+                        app.log(obj + ' -> ' + method)
+                    }
+                } (),
+                object, i);
+            }
+        }
+    }
+});
---
+++ app/views/Viewport.js
@@ -0,0 +1,50 @@
+/**
+*    Viewport.js
+*    	
+*    Created by Jerzy Błaszczyk on 2011-09-01.
+*    Copyright 2011 Client and Friends. All rights reserved.
+**/
+
+app.views.Viewport = Ext.extend(Ext.TabPanel, {
+
+    fullscreen: true,
+    layout: 'card',
+    cardSwitchAnimation: 'slide',
+    tabBar: {
+        dock: 'bottom',
+        ui: 'dark',
+        layout: {
+            pack: 'center'
+        }
+    },
+
+    listeners: {
+        scope: this,
+        cardswitch: function(me, newTab, oldTab) {
+            app.currentTab = newTab;
+            if (oldTab) {
+                oldTab.destroy();
+            }
+        }
+    },
+
+    initComponent: function() {
+
+        app.views.firstTab = new app.views.Tab({
+            name: 'FirstTab'
+        });
+        app.views.secondTab = new app.views.Tab({
+            name: 'SecondTab'
+        });
+
+        app.aop(app.views.firstTab);
+        app.aop(app.views.secondTab);
+
+        this.items = [app.views.firstTab, app.views.secondTab];
+
+        app.views.Viewport.superclass.initComponent.apply(this, arguments);
+
+        app.currentTab = app.views.firstTab;
+    }
+
+});
